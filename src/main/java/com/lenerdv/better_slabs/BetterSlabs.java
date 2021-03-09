1455879a6040d90b1ecad03ee60f11bd7742aa28
lenerdv2005 <lenerdv2005@gmail.com>
2021-03-09 22:30:40 +0100

    initial skeleton for the mod

---
+++ src/main/java/com/lenerdv/better_slabs/BetterSlabs.java
@@ -0,0 +1,9 @@
+package com.lenerdv.better_slabs;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BetterSlabs.modId)
+public class BetterSlabs {
+	public static final String modId = "better_slabs";
+	public static final String version = "1.0.0";
+}
